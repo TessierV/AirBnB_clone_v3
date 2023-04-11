6cfb0d8dc54a452b0b0b0064e07341871b8d9d6e
TessierV <5628@holbertonstudents.com>
2023-04-11 09:01:12 -0700

    test

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-""" Index file """
+""" index """
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
+    """ return the status of the API """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def app_views_stats():
-    """ Create an endpoint that retrieves
-    the number of each objects by type """
-    stats_obj = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User')
+def number_obj():
+    """ retrieves the number of objects by type """
+    count_obj = {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
     }
-    return jsonify(stats_obj)
+    return jsonify(count_obj)
