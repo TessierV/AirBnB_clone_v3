a92a01247a18191ef0e70edc55b6b11b6b26dfdf
TessierV <5628@holbertonstudents.com>
2023-04-11 05:47:41 -0700

    test localhost

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,12 @@
 """ API Flask app module """
 from flask import Flask
 from models import storage
-
 from api.v1.views import app_views
 import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
@@ -22,4 +21,3 @@ if __name__ == "__main__":
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=os.getenv('HBNB_API_PORT', '5000'),
             threaded=True)
-
