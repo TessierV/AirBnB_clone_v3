5ae541a623a22a4316e796aadc54daa376b296dc
TessierV <5628@holbertonstudents.com>
2023-04-11 07:42:22 -0700

    fix documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,7 +7,9 @@ from models import storage
 
 @app_views.route('/stats', strict_slashes=False)
 def app_views_stats():
-    stats = {
+    """ Create an endpoint that retrieves
+    the number of each objects by type """
+    stats_obj = {
         'amenities': storage.count('Amenity'),
         'cities': storage.count('City'),
         'places': storage.count('Place'),
@@ -15,7 +17,7 @@ def app_views_stats():
         'states': storage.count('State'),
         'users': storage.count('User')
     }
-    return jsonify(stats)
+    return jsonify(stats_obj)
 
 
 @app_views.route("/status", strict_slashes=False)
