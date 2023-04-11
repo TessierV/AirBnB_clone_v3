503008a6788ecc17ec0690b191e3958b90af3bcb
TessierV <5628@holbertonstudents.com>
2023-04-11 06:17:58 -0700

    fix error with support

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Index file """
+from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route("/status")
+@app_views.route("/status", strict_slashes=False)
 def status():
-    return '{"status": "OK"}'
+    return jsonify({"status": "OK"})
